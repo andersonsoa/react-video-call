30750263e0720a1cf0ee478aa87e031f104b3cd9
Anderson Soares <anderson.ceu@gmail.com>
2023-01-20 16:47:14 -0400

    env

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SocketModule } from './socket/socket.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [SocketModule, DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SocketModule,
+    DatabaseModule,
+  ],
 })
 export class AppModule {}
